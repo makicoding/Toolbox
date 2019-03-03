eee43a76d0c6829e90a0b271008aa1f373d1f2d2
Maki Hirose <Capture@Makis-MBP-RETINA-13.local>
2019-03-03 11:59:27 -0500

    190303 11:59pm Commit

---
+++ JavaScript/13 JavaScript - Example of how to write an array of objects/arrayofobjects.js
@@ -0,0 +1,37 @@
+var qa = [
+    {
+        question:   "What was the name of the lead character in The Matrix?",
+        correct:    "Neo",
+        incorrect1: "Trinity",
+        incorrect2: "Morpheus",
+        incorrect3: "Agent Smith"   
+    },
+    {
+        question:   "In American Pie, what subject does Nadia ask Jim to help her study for?",
+        correct:    "World History",
+        incorrect1: "Biology",
+        incorrect2: "Mathematics",
+        incorrect3: "English"   
+    },
+    {
+        question:   "In which movie did Tom Cruise play the role of Ethan Hunt?",
+        correct:    "Mission Impossible",
+        incorrect1: "Jerry Maguire",
+        incorrect2: "Eyes Wide Shut",
+        incorrect3: "Days of Thunder"   
+    },
+    {
+        question:   "What year was Forrest Gump released?",
+        correct:    "1994",
+        incorrect1: "1991",
+        incorrect2: "1992",
+        incorrect3: "1993"   
+    },
+    {
+        question:   "What was Adam Sandler's occupation in Big Daddy?",
+        correct:    "Tollbooth worker",
+        incorrect1: "Teacher",
+        incorrect2: "Car Salesman",
+        incorrect3: "Pro Hockey Player"   
+    }
+];
