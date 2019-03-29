23ef15170ca6b59ed4e96b009f982e2fe2f1608a
Maki Hirose <Capture@Makis-MBP-RETINA-13.local>
2019-03-29 18:21:28 -0400

    190329 6:21pm Commit

---
+++ CSS/04 CSS - Menus/05 Left and Right Side Navigation Menu/Version 02/leftAndRightSideNavigationMenuV2.js
@@ -0,0 +1,17 @@
+console.log ("JavaScript Connected!");
+
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+}
+  
+function closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+}
+  
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+}
+  
+function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+}
